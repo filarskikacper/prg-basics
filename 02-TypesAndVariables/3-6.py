edca62cf11ed46d900fa0bc418ea2b5fcf518a02
Kacper Filarski <kacperkof9@gmail.com>
2024-10-08 11:37:26 +0200

    Aktualizacja

---
+++ 02-TypesAndVariables/3-6.py
@@ -0,0 +1,4 @@
+import math
+h = float(input('Enter your height in meters: '))
+d = 3.57 * math.sqrt(h)
+print(d)
---
+++ 02-TypesAndVariables/4-1.py
@@ -0,0 +1,6 @@
+age = 19
+height = 182
+
+print(f"My name is Adam.")
+print(f"I am {age} years old, and my height is {height} cm.")
+print(f"In 6 years I will be {age+1} years old.")
