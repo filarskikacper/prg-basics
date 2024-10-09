bc5fb24b227b068e3b5b5966aad86c133889e712
Kacper Filarski <kacperkof9@gmail.com>
2024-10-09 23:55:31 +0200

    All done

---
+++ 02-TypesAndVariables/8-4.py
@@ -0,0 +1,9 @@
+###
+# A program that prints your height both in cm and in feet and inches.
+#
+cm = 182
+feet = 0.0328 * cm
+inches = 1 / 2.54 * cm
+# calculate the number of feet
+
+print(f'I am {cm}cm tall, i.e. {feet}feet and {inches}inches')
