96376a120373ae114689620166552fa2a722558d
Kacper Filarski <kacperkof9@gmail.com>
2024-10-15 10:49:11 +0200

    Aktualizacja

---
+++ 03-ControlStructures/2-6.py
@@ -0,0 +1,8 @@
+number = int(input('Enter number: '))
+
+if number > 0:
+    print(f'Number {number} is positive')
+elif number == 0:
+    print(f'Number {number} is 0')
+else:
+    print(f'Number {number} is negative')
